10a3a79896fdb36aebcbf545ae6a49d498acae38
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2021-09-18 12:00:25 +0700

    Update fds.sh

--- fds.sh
+++ fds.sh
@@ -3,10 +3,8 @@ wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/nu
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p "Ctrl + V Authtoken: " CRP 
 ./ngrok authtoken $CRP 
-nohup ./ngrok tcp 4000 &>/dev/null &
+nohup ./ngrok tcp 3389 &>/dev/null &
 echo Downloading File From akuh.net
 apt update && apt upgrade
 apt install ubuntu-desktop -y
-wget -O nomachine.deb https://bit.ly/nomachinev7
-sudo apt install ./nomachine.deb
 ! curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
