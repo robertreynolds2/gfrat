f0bcd231617aa203d5645b5513871255f0ab7451
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2021-09-18 11:38:33 +0700

    Create fds.sh

---
+++ fds.sh
@@ -0,0 +1,12 @@
+echo "Download ngrok"
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+read -p "Ctrl + V Authtoken: " CRP 
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp 4000 &>/dev/null &
+echo Downloading File From akuh.net
+apt update && apt upgrade
+apt install ubuntu-desktop -y
+wget -O nomachine.deb https://bit.ly/nomachinev7
+sudo apt install ./nomachine.deb
+! curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
